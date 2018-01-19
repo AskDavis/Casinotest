bf69de0c94208545c07b0393b004b2f7b51bf682
jacobbdavis1991 <35181018+jacobbdavis1991@users.noreply.github.com>
2018-01-19 10:53:10 -0800

    Update memenv.h

--- src/leveldb/helpers/memenv/memenv.h
+++ src/leveldb/helpers/memenv/memenv.h
@@ -4,17 +4,4 @@
 
 #ifndef __STDC_LIMIT_MACROS
 #define __STDC_LIMIT_MACROS
-
-namespace leveldb {
-
-class Env;
-
-// Returns a new environment that stores its data in memory and delegates
-// all non-file-storage tasks to base_env. The caller must delete the result
-// when it is no longer needed.
-// *base_env must remain live while the result is in use.
-Env* NewMemEnv(Env* base_env);
-
-}  // namespace leveldb
-
-#endif  // STORAGE_LEVELDB_HELPERS_MEMENV_MEMENV_H_
+#endif
